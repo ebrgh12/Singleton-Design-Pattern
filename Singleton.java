ad6e46a683d988ca32e4a8465c2b2cac1fbd5cbe
Girish <girish12ramesh@gmail.com>
2017-12-02 16:14:21 -0800

    feat: Synchronized Added a thread safe using Synchronized

--- Singleton.java
+++ Singleton.java
@@ -1,33 +1,20 @@
-/* Here we have declared getInstance() static so 
- * that we can call it without instantiating the class. 
- * The first time getInstance() is called it creates a new singleton object and after that it just returns the same object. 
- * Note that Singleton obj is not created until we need it and call getInstance() method. This is called lazy instantiation.
+package SingletonSynchronized;
 
-The main problem with above method is that it is not thread safe. Consider the following execution sequence.
-This execution sequence creates two objects for singleton. Therefore this classic implementation is not thread safe.
-
-*/
+/* Here using synchronized makes sure that only one thread at a time can execute getInstance()*/
 
 public class Singleton {
-	
+
 	private static Singleton obj;
-
-// Private constructor to force use of 
-// getInstance() to create Singleton object
-    private Singleton()
-	{
 	
+	// Private Constructor
+	private Singleton(){
+		
 	}
-
-public static Singleton getInstance(){
-	if(obj==null){
-		obj=new Singleton();
+	// Only one thread can execute this at a time as we have used Synchronized
+	public static synchronized Singleton getInstance(){
+		if(obj==null){
+			obj=new Singleton();
+		}
+		return obj;
 	}
-	return obj;
- }
-public void showMessage(){
-	System.out.println("Hello Singleton");
 }
-
-}
- 
